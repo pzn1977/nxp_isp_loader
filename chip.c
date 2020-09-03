8dd900d4ed43b79fcc27f9d50d91847a8d4cc98e
Pedro Zorzenon Neto <pedro@pzn.com.br>
2020-09-03 17:51:21 -0300

    added LPC177x and LPC178x

--- chip.c
+++ chip.c
@@ -12,27 +12,82 @@ uint32_t chip_ram_transfer_addr = 0;
 char * chip_flash_configure (uint8_t * s) {
   char * ret = NULL;
 
-  /* LPC1115 ID is 0x00050080 = 327808(decimal) */
-  if (strcmp("327808",(char*)s) == 0) {
-    int i;
-    /* chip has 16 sectors of 4 kbytes each (all sectors with same size) */
-    ret = strdup("LPC1115");
-    chip_sector_max = 15; /* last sector number */
+  if (strcmp("654381362",(char*)s) == 0) {
+    /* https://www.nxp.com/docs/en/user-guide/UM10470.pdf
+     * Section 37.5 Sector numbers */
+    ret = strdup("LPC1774");
+    chip_sector_max = 17; /* last sector number */
     chip_sector = malloc(sizeof(chip_sector)*(chip_sector_max+1));
-    for (i=0; i<=chip_sector_max; i++) {
-      chip_sector[i] = 0x1000 * i; /* initial address of each sector */
-    }
+    chip_sector[0] =  0x00000;
+    chip_sector[1] =  0x01000;
+    chip_sector[2] =  0x02000;
+    chip_sector[3] =  0x03000;
+    chip_sector[4] =  0x04000;
+    chip_sector[5] =  0x05000;
+    chip_sector[6] =  0x06000;
+    chip_sector[7] =  0x07000;
+    chip_sector[8] =  0x08000;
+    chip_sector[9] =  0x09000;
+    chip_sector[10] = 0x0a000;
+    chip_sector[11] = 0x0b000;
+    chip_sector[12] = 0x0c000;
+    chip_sector[13] = 0x0d000;
+    chip_sector[14] = 0x0e000;
+    chip_sector[15] = 0x0f000;
+    chip_sector[16] = 0x10000;
+    chip_sector[17] = 0x18000;
     chip_addr_min = 0; /* first address of flash memory */
-    chip_addr_max = 0xffff; /* last address of flash memory */
+    chip_addr_max = 0x1ffff; /* last address of flash memory */
     chip_ram_transfer_addr = 0x10000300; /* a start address of free 512 bytes of RAM to be used as buffer to transfer pages */
   }
 
-  /* LPC1788 ID is 0x281d3f47 = 673005383(decimal) */
-  if (strcmp("673005383",(char*)s) == 0) {
-    /* chip has 16 sectors of 4 kbytes, and 16 sectors of 32 kbytes
-     * https://www.nxp.com/docs/en/user-guide/UM10470.pdf
+  if ((strcmp("672997187",(char*)s) == 0) ||
+      (strcmp("672995139",(char*)s) == 0) ||
+      (strcmp("655957827",(char*)s) == 0)) {
+    /* https://www.nxp.com/docs/en/user-guide/UM10470.pdf
      * Section 37.5 Sector numbers */
-    ret = strdup("LPC1788");
+    if (strcmp("672997187",(char*)s) == 0) ret = strdup("LPC1786");
+    if (strcmp("672995139",(char*)s) == 0) ret = strdup("LPC1785");
+    if (strcmp("655957827",(char*)s) == 0) ret = strdup("LPC1776");
+    chip_sector_max = 21; /* last sector number */
+    chip_sector = malloc(sizeof(chip_sector)*(chip_sector_max+1));
+    chip_sector[0] =  0x00000;
+    chip_sector[1] =  0x01000;
+    chip_sector[2] =  0x02000;
+    chip_sector[3] =  0x03000;
+    chip_sector[4] =  0x04000;
+    chip_sector[5] =  0x05000;
+    chip_sector[6] =  0x06000;
+    chip_sector[7] =  0x07000;
+    chip_sector[8] =  0x08000;
+    chip_sector[9] =  0x09000;
+    chip_sector[10] = 0x0a000;
+    chip_sector[11] = 0x0b000;
+    chip_sector[12] = 0x0c000;
+    chip_sector[13] = 0x0d000;
+    chip_sector[14] = 0x0e000;
+    chip_sector[15] = 0x0f000;
+    chip_sector[16] = 0x10000;
+    chip_sector[17] = 0x18000;
+    chip_sector[18] = 0x20000;
+    chip_sector[19] = 0x28000;
+    chip_sector[20] = 0x30000;
+    chip_sector[21] = 0x38000;
+    chip_addr_min = 0; /* first address of flash memory */
+    chip_addr_max = 0x3ffff; /* last address of flash memory */
+    chip_ram_transfer_addr = 0x10000300; /* a start address of free 512 bytes of RAM to be used as buffer to transfer pages */
+  }
+
+  if ((strcmp("673005383",(char*)s) == 0) ||
+      (strcmp("673003335",(char*)s) == 0) ||
+      (strcmp("655966023",(char*)s) == 0) ||
+      (strcmp("655963975",(char*)s) == 0)) {
+    /* https://www.nxp.com/docs/en/user-guide/UM10470.pdf
+     * Section 37.5 Sector numbers */
+    if (strcmp("673005383",(char*)s) == 0) ret = strdup("LPC1788");
+    if (strcmp("673003335",(char*)s) == 0) ret = strdup("LPC1787");
+    if (strcmp("655966023",(char*)s) == 0) ret = strdup("LPC1778");
+    if (strcmp("655963975",(char*)s) == 0) ret = strdup("LPC1777");
     chip_sector_max = 29; /* last sector number */
     chip_sector = malloc(sizeof(chip_sector)*(chip_sector_max+1));
     chip_sector[0] =  0x00000;
@@ -67,6 +122,21 @@ char * chip_flash_configure (uint8_t * s) {
     chip_sector[29] = 0x78000;
     chip_addr_min = 0; /* first address of flash memory */
     chip_addr_max = 0x7ffff; /* last address of flash memory */
+    chip_ram_transfer_addr = 0x10000300; /* a start address of free 512 bytes of RAM to be used as buffer to transfer pages */
+  }
+
+  /* LPC1115 ID is 0x00050080 = 327808(decimal) */
+  if (strcmp("327808",(char*)s) == 0) {
+    int i;
+    /* chip has 16 sectors of 4 kbytes each (all sectors with same size) */
+    ret = strdup("LPC1115");
+    chip_sector_max = 15; /* last sector number */
+    chip_sector = malloc(sizeof(chip_sector)*(chip_sector_max+1));
+    for (i=0; i<=chip_sector_max; i++) {
+      chip_sector[i] = 0x1000 * i; /* initial address of each sector */
+    }
+    chip_addr_min = 0; /* first address of flash memory */
+    chip_addr_max = 0xffff; /* last address of flash memory */
     chip_ram_transfer_addr = 0x10000300; /* a start address of free 512 bytes of RAM to be used as buffer to transfer pages */
   }
 
